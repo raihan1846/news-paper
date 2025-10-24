f275d35340d2205d9f114bffc07e056804e7932d
raihan1846 <raihan.bd2022@gmail.com>
2025-10-24 19:51:29 +0600

    loading add

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -9,6 +9,7 @@ import Register from '../pages/Register';
 import AuthLayout from '../layouts/AuthLayout';
 import NewsDetails from '../pages/NewsDetails';
 import PrivateRoute from '../provider/PrivateRoute';
+import Loading from '../pages/Loading';
 
 const router = createBrowserRouter([
     {
@@ -22,7 +23,8 @@ const router = createBrowserRouter([
         {
             path: "category/:id",
             element: <CategoryNews></CategoryNews>,
-            loader: () => fetch("/news.json")
+            loader: () => fetch("/news.json"),
+            hydrateFallbackElement: <Loading></Loading>
         }
       ]
     },
@@ -45,7 +47,8 @@ const router = createBrowserRouter([
         element: <PrivateRoute>
             <NewsDetails></NewsDetails>
         </PrivateRoute>,
-        loader: ()=> fetch("/news.json")
+        loader: ()=> fetch("/news.json"),
+        hydrateFallbackElement: <Loading></Loading>
       },
       {
         path: "/*",
