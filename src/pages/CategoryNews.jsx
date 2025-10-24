059606bc673c0355779431332bf8cf43ff1f4e0f
raihan1846 <raihan.bd2022@gmail.com>
2025-10-24 11:49:52 +0600

    category wise news count , connect all news using api

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const CategoryNews = () => {
     const {id} = useParams();
-    // console.log(obj);
+    const data = useLoaderData();
+    const [categoryNews, setCategoryNews] = useState([]);
+
+    useEffect(()=>{
+        if(id == "0"){
+        setCategoryNews(data);
+        return;
+        }else if(id == "1"){
+            const filteredNews = data.filter(news => news.others.is_today_pick == true);
+            // console.log(filteredNews);
+            setCategoryNews(filteredNews);
+        }else{
+            const filteredNews = data.filter(news => news.category_id == id);
+            // console.log(filteredNews);
+    
+            setCategoryNews(filteredNews);
+        }
+       
+        
+    },[data, id])
+    // console.log(data);
     return (
         <div>
-            CategoryNews - {id}
+            Total ({categoryNews.length}) News Found
         </div>
     );
 };
